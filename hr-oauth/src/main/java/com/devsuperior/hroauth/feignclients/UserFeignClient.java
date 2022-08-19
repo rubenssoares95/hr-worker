71147a4a5abc597603f94e325fbb8b9373d57a26
Rubens <rubenssoares95@gmail.com>
2022-08-19 00:33:29 -0300

    User Feign Client created

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/feignclients/UserFeignClient.java
@@ -0,0 +1,18 @@
+package com.devsuperior.hroauth.feignclients;
+
+import com.devsuperior.hroauth.entities.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Component
+@FeignClient(name = "hr-user", path = "/users")
+public interface UserFeignClient {
+
+    @GetMapping("/search")
+    ResponseEntity<User> findByEmail(@RequestParam String email);
+
+}
